e8bfcdbd4f4e1ef07ffc04ec29b0e769dc19138c
Lynne Joanne Williams <williams.lynne99@gmail.com>
2020-07-29 11:34:42 -0700

    Fixed typo and added loop to run through fmap folders when n > 1

--- fieldmap_preproc.sh
+++ fieldmap_preproc.sh
@@ -35,37 +35,65 @@ do
 
   # Change directories into the fmap directory
   cd ./${subject}/fmap/
+
+  # remove any previously split files from the directory
+  rm *split*
   
-  # get the nifti file for the fieldmap
+  # get the nifti file(s) for the fieldmap(s)
   string=`echo sub*_fieldmap.nii.gz`
-  echo "    ${string}"
 
-  # split the file into its 2 volumes
-  fslsplit ${string}
+  # check the length of the string(s) and 
+  # loop through multiple fieldmaps,
+  # if necessary
 
-  # break the string apart and assign it to an array
-  IFS='_' read -r -a array <<< "${string}"
-
-  # copy the files to their correct BIDS naming conventions 
-  # (using AFNI to keep the history)
-  3dcopy vol0000.nii.gz ${array[0]}_${array[1]}-split_fieldmap.nii.gz
-  3dcopy vol0001.nii.gz ${array[0]}_${array[1]}-split_magnitude.nii.gz
-
-  # and copy the json file to the new name
-  cp ${array[0]}_${array[1]}_fieldmap.json \
-     ${array[0]}_${array[1]}-split_fieldmap.json
+  # get the length of the string array
+  IFS=' ' read -r -a strings <<< "${string}"
   
-  # now remove the wrongly named files
-  rm vol000*
+  length=${#strings[@]}
+
+  # loop through the strings (this will catch 
+  # when there is more than one fieldmap in 
+  # the fmap directory)
+  for (( i=0; i<$length; i++ ))
+  do 
+    echo "++ ${strings[$i]}" 
+
+    # split the file into its 2 volumes
+    fslsplit ${strings[$i]}
+
+    # break the string apart and assign it to an array
+    IFS='_' read -r -a array <<< "${strings[$i]}"
+
+    # copy the files to their correct BIDS naming conventions 
+    # (using AFNI to keep the history)
+
+    # get the run number
+    printf -v run "%02d" $(( $i + 1 ))
+
+    # write the nifti files
+    3dcopy vol0000.nii.gz \
+           ${array[0]}_${array[1]}-split_run-${run}_fieldmap.nii.gz
+    3dcopy vol0001.nii.gz \
+           ${array[0]}_${array[1]}-split_run-${run}_magnitude.nii.gz
+
+    # and copy the json file to the new name
+    cp ${strings[$i]%.nii.gz}.json \
+       ${array[0]}_${array[1]}-split_run-${run}_fieldmap.json
+  
+    # now remove the wrongly named files
+    rm vol000*
+
+    # Now append the original fieldmap and json to the bidsignore file
+    echo ${strings[$i]} >> ${1}/.bidsignore
+    echo ${strings[$i]%.nii.gz}.json >> ${1}/.bidsignore
+  done
 
   # go back to the base directory
   cd ${1}
 
-  # Now append the original fieldmap and json to the bidsignore file
-  echo ${string%nii.gz}* >> .bidsignore
-
 done
 
+# go back to the directory you started in
 cd ${2}
 
 echo "All done!"
