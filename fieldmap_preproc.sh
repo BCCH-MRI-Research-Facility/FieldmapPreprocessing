c0bb321950371490f5c3d0eb003b0653a4e20124
Lynne <Williams>
2020-08-03 17:30:26 -0700

    updating code to ignore already processed fieldmaps

--- fieldmap_preproc.sh
+++ fieldmap_preproc.sh
@@ -24,10 +24,15 @@
 # - sub-<subjectid>_acq-b0map-split_magnitude.nii.gz
 
 
+# set dTEinv and FWHM.  Check your data and set these 
+# to appropriate values for your data.
+dTEinv=217 # dTEinv is inverse of the TE difference (in ms) in the GRE sequence
+FWHM=3 # set width of kernel for erroding the brain mask (should be between 1 and 3 mm)
 
 # change directories into the root of your BIDS directory
 # get the base directory path
 cd ${1}
+
 
 for subject in `ls -d sub-*`
 do
@@ -40,88 +45,101 @@ do
   # Change directories into the fmap directory
   cd ./${subject}/fmap/
 
-  # remove any previously split files from the directory
-  rm *split*
+  # remove and reprocess split files on request
+  if [ ${3} == overwrite ]
+  then
+    # remove any previously split files from the directory
+    rm *split*
+  fi
+
+  # Check if proper fieldmaps exist
+  if [ ! -f *_magnitude.nii.gz ]
+  then
   
-  # get the nifti file(s) for the fieldmap(s)
-  string=`echo sub*_fieldmap.nii.gz`
+    # get the nifti file(s) for the fieldmap(s)
+    string=`echo sub*_fieldmap.nii.gz`
 
-  # check the length of the string(s) and 
-  # loop through multiple fieldmaps,
-  # if necessary
+    # check the length of the string(s) and 
+    # loop through multiple fieldmaps,
+    # if necessary
 
-  # get the length of the string array
-  IFS=' ' read -r -a strings <<< "${string}"
+    # get the length of the string array
+    IFS=' ' read -r -a strings <<< "${string}"
   
-  length=${#strings[@]}
+    length=${#strings[@]}
 
-  # loop through the strings (this will catch 
-  # when there is more than one fieldmap in 
-  # the fmap directory)
-  for (( i=0; i<$length; i++ ))
-  do 
-    echo "++ ${strings[$i]}" 
+    # loop through the strings (this will catch 
+    # when there is more than one fieldmap in 
+    # the fmap directory)
+    for (( i=0; i<$length; i++ ))
+    do 
+      echo "++ ${strings[$i]}" 
 
-    # split the file into its 2 volumes
-    fslsplit ${strings[$i]}
+      # split the file into its 2 volumes
+      fslsplit ${strings[$i]}
     
-    # break the string apart and assign it to an array
-    IFS='_' read -r -a array <<< "${strings[$i]}"
+      # break the string apart and assign it to an array
+      IFS='_' read -r -a array <<< "${strings[$i]}"
     
-    echo "++++ First the Magnitude Image"
-    # run the FSL Brain Extraction Tool (BET) on the magnitude image (vol0001.nii.gz)
-    bet vol0001 vol0001_brain -R -m
+      echo "++++ First the Magnitude Image"
+      # run the FSL Brain Extraction Tool (BET) on the magnitude image
+      # (vol0001.nii.gz). Regular BET not working well due to bias.  Doing
+      # bias correction too.
+      # bet vol0001 vol0001_brain -R -m
+      bet vol0001 vol0001_brain -B -f 0.55 -g 0 -m
+
+      # errode the brain mask. Choose FWHM between 1-3 mm appropriately so that mask is slightly smaller than the brain image
+      fslmaths vol0001_brain_mask -kernel gauss ${FWHM} -ero vol0001_brain_mask
     
-    # errode the brain mask. Choose FWHM between 1-3 mm appropriately so that mask is slightly smaller than the brain image
-    fslmaths vol0001_brain_mask -kernel gauss 1 -ero vol0001_brain_mask
+      # remask vol0001.nii.gz with eroded brain mask 
+      fslmaths vol0001 -mas vol0001_brain_mask vol0001_brain
     
-    # remask vol0001.nii.gz with eroded brain mask 
-    fslmaths vol0001 -mas vol0001_brain_mask vol0001_brain
+      echo "++++ Now the Phase-difference Image (this takes awhile. It might be a good time for coffee)"
+      # The phase difference image is in units of Hz, and is 
+      # phase-wrapped. To unwrap, normalize phase difference 
+      # image to [-pi,pi] range. **dTEinv is inverse of the 
+      # TE difference (in ms) in the GRE sequence. Usually 
+      # dTEinv = 217 **. The default value of dTEinv of 
+      # 217 is used here.  Change this value if different 
+      # for your acquisition.
+      fslmaths vol0000 -mul 3.1415 -div ${dTEinv} vol0000_rad
     
-    echo "++++ Now the Phase-difference Image (this takes awhile. It might be a good time for coffee)"
-    # The phase difference image is in units of Hz, and is 
-    # phase-wrapped. To unwrap, normalize phase difference 
-    # image to [-pi,pi] range. **dTEinv is inverse of the 
-    # TE difference (in ms) in the GRE sequence. Usually 
-    # dTEinv = 217 **. The default value of dTEinv of 
-    # 217 is used here.  Change this value if different for your acquisition.
-    fslmaths vol0000 -mul 3.1415 -div 217 vol0000_rad
+      # phase_rad is now in units of radians. Unwrap phase_rad using FSL PRELUDE 
+      prelude -a vol0000 -p vol0000_rad -o vol0000_rad_unwrapped
     
-    # phase_rad is now in units of radians. Unwrap phase_rad using FSL PRELUDE 
-    prelude -a vol0000 -p vol0000_rad -o vol0000_rad_unwrapped
+      # convert phase_rad_unwrapped into rad/s units dTEinv = 217
+      fslmaths vol0000_rad_unwrapped -mul ${dTEinv} vol0000_rad_unwrapped_rps
     
-    # convert phase_rad_unwrapped into rad/s units 
-    fslmaths vol0000_rad_unwrapped -mul dTEinv vol0000_rad_unwrapped_rps
-    
-    #regularlize phase_rad_unwrapped_rps using FUGUE. As a note, 
-    # different regularization methods exists within FUGUE. Note FWHM=5.
-    # Change if needed for your data
-    fugue --loadfmap=vol0000_rad_unwrapped_rps.nii.gz \
-       -s 5 --savefmap=vol0000_fieldmap.nii.gz
+      #regularlize phase_rad_unwrapped_rps using FUGUE. As a note, 
+      # different regularization methods exists within FUGUE. Note FWHM=5.
+      # Change if needed for your data
+      fugue --loadfmap=vol0000_rad_unwrapped_rps.nii.gz \
+         -s 5 --savefmap=vol0000_fieldmap.nii.gz
     
 
-    # copy the files to their correct BIDS naming conventions 
-    # (using AFNI to keep the history)
+      # copy the files to their correct BIDS naming conventions 
+      # (using AFNI to keep the history)
 
-    # get the run number
-    printf -v run "%02d" $(( $i + 1 ))
+      # get the run number
+      printf -v run "%02d" $(( $i + 1 ))
 
-    # write the nifti files
-    3dcopy vol0000_fieldmap.nii.gz \
-           ${array[0]}_${array[1]}split_run-${run}_fieldmap.nii.gz
-    3dcopy vol0001_brain.nii.gz \
-           ${array[0]}_${array[1]}split_run-${run}_magnitude.nii.gz
+      # write the nifti files
+      3dcopy vol0000_fieldmap.nii.gz \
+             ${array[0]}_${array[1]}split_run-${run}_fieldmap.nii.gz
+      3dcopy vol0001_brain.nii.gz \
+             ${array[0]}_${array[1]}split_run-${run}_magnitude.nii.gz
 
-    # and copy the json file to the new name
-    cp ${strings[$i]%.nii.gz}.json \
-       ${array[0]}_${array[1]}split_run-${run}_fieldmap.json
+      # and copy the json file to the new name
+      cp ${strings[$i]%.nii.gz}.json \
+         ${array[0]}_${array[1]}split_run-${run}_fieldmap.json
   
-    # now remove the wrongly named files
-    rm vol000*
+      # now remove the wrongly named files
+      rm vol000*
 
-    # Now append the original fieldmap and json to the bidsignore file
-    echo ${strings[$i]} >> ${1}/.bidsignore
-    echo ${strings[$i]%.nii.gz}.json >> ${1}/.bidsignore
+      # Now append the original fieldmap and json to the bidsignore file
+      echo ${strings[$i]} >> ${1}/.bidsignore
+      echo ${strings[$i]%.nii.gz}.json >> ${1}/.bidsignore
+    fi
   done
 
   # go back to the base directory
